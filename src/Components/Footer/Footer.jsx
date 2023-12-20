18faa583235ce4f17cf08339dd50e8aaa1e58ee4
Ehab-Tarek <ehabtarek166@gmail.com>
2023-12-20 15:34:19 +0200

    Handle Footer.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import styles from './Footer.module.css'
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <>
+            <footer className=' fixed-bottom bg-dark py-5 text-center text-white '>
+                <h2>Footer</h2>
+            </footer>
+
+        </>
     )
 }
