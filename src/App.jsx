c233da64acaec8f3516c2e60edddea854744cfdd
Ehab-Tarek <ehabtarek166@gmail.com>
2023-12-20 15:32:55 +0200

    Add All Components ,Handle navBar,Provide Routes To The App .

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import LayOut from './Components/LayOut/LayOut'
+import Home from './Components/Home/Home'
+import Products from './Components/Products/Products'
+import Categories from './Components/Categories/Categories'
+import Cart from './Components/Cart/Cart'
+import Register from './Components/Register/Register'
+import LogIn from './Components/LogIn/LogIn'
+import NotFound from './Components/NotFound/NotFound'
+import Brands from './Components/Brands/Brands'
+
+export default function App() {
+
+
+
+  const routes = createBrowserRouter([{
+    path: '', element: <LayOut />, children: [
+      { index: true, element: <Home /> },
+      { path: 'products', element: <Products /> },
+      { path: 'categories', element: <Categories /> },
+      { path: 'cart', element: <Cart /> },
+      { path: 'brands', element: <Brands /> },
+      { path: 'register', element: <Register /> },
+      { path: 'login', element: <LogIn /> },
+
+      { path: '*', element: <NotFound /> }
+    ]
+  }])
+  return (
+    <>
+      <RouterProvider router={routes}></RouterProvider>
+    </>
+  )
+}
---
+++ src/Components/LogIn/LogIn.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './LogIn.module.css'
+export default function LogIn() {
+    return (
+        <div>LogIn</div>
+    )
+}
